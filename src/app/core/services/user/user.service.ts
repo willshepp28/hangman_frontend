59cf374205947f3e65a21806f3108495a2209f0e
willshepp28 <willsheppard28@outlook.com>
2018-10-25 15:56:17 -0400

    user info now population on home page

--- src/app/core/services/user/user.service.ts
+++ src/app/core/services/user/user.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  private url = "http://localhost:3000/api/v1/user/";
+
+  constructor(private http: HttpClient) { }
+
+
+/*
+|--------------------------------------------------------------------------
+| GET - gets one user profile data
+|--------------------------------------------------------------------------
+*/
+getUser(userId) {
+  return this.http.get<any>(this.url + userId);
+}
+
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+
+// Services
 import { DecodeTokenService } from '../core/helpers/decodeToken/decode-token.service';
+import { UserService } from '../core/services/user/user.service';
 
 @Component({
   selector: 'app-home',
@@ -8,13 +12,27 @@ import { DecodeTokenService } from '../core/helpers/decodeToken/decode-token.ser
 })
 export class HomeComponent implements OnInit {
 
+  username: string;
+
   constructor(
-    private decodeToken: DecodeTokenService
+    private route: ActivatedRoute,
+    private decodeToken: DecodeTokenService,
+    private userService: UserService
   ) { }
 
   ngOnInit() {
 
     // we need to get the user name
+    const token = this.decodeToken.getDecodedAccessToken(localStorage.getItem('token'));
+    const tokenId = token.user[0].id;
+
+    console.log(tokenId);
+
+    this.userService.getUser(tokenId)
+      .subscribe(
+        response => { console.log(response), this.username = response[0].username; },
+        error => { console.log(error);}
+      );
   }
 
 }
