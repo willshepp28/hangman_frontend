c5aa9929d367f47fe3522652e9de1d3c3668bcb6
willshepp28 <willsheppard28@outlook.com>
2018-10-28 11:53:25 -0400

    fixed routelink problem with account/:id

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { AuthService } from './core/authentication/auth.service';
 import { DecodeTokenService } from './core/helpers/decodeToken/decode-token.service';
-import { UserService } from './core/services/user/user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  title = 'app';
+export class AppComponent {
   token;
   userId;
 
   constructor(
     private decodeToken: DecodeTokenService,
-    private userService: UserService,
     private authService: AuthService
   ) { }
 
-  ngOnInit() {
-    // we need to get the user name
+  getId() {
     this.token = this.decodeToken.getDecodedAccessToken(localStorage.getItem('token'));
     this.userId = this.token.user[0].id;
+    return this.userId;
   }
-
 
   isAuthenticated() {
     return this.authService.isLoggedIn();
