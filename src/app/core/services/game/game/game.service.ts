684aa404147e4e36cceef6a3c0dd85bb15854fa1
willshepp28 <willsheppard28@outlook.com>
2018-11-01 21:25:50 -0400

    changed url back

--- src/app/core/services/game/game/game.service.ts
+++ src/app/core/services/game/game/game.service.ts
@@ -9,8 +9,8 @@ import { environment } from 'src/environments/environment.prod';
 
 export class GameService {
 
-  private url2 = "https://hangman-backend.herokuapp.com/api/v1/game/";
-  private url = "http://localhost:3000/api/v1/game/";
+  private url = "https://hangman-backend.herokuapp.com/api/v1/game/";
+  private ur2 = "http://localhost:3000/api/v1/game/";
 
   constructor(private http: HttpClient) { }
 
